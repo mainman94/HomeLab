fc9a66534ddf19095faedc5d60e26f1e5be14275
Philipp Hauptmann <haupha08@gmail.com>
2025-04-25 07:49:23 +0200

    Added ha kubeadm

--- terraform/cluster/files.tf
+++ terraform/cluster/files.tf
@@ -9,15 +9,15 @@ resource "proxmox_virtual_environment_file" "cloud_config" {
 }
 
 resource "local_file" "ansible_inventory" {
-  count = length(proxmox_virtual_environment_vm.k8s_cp) > 0 && length(proxmox_virtual_environment_vm.k8s_worker) > 0 ? 1 : 0
+#  count = length(proxmox_virtual_environment_vm.k8s_cp) > 0 && length(proxmox_virtual_environment_vm.k8s_worker) > 0 ? 1 : 0
   filename = var.ansible_k8s_cluster_inventory_path
 
-  content = <<-EOF
+content = <<-EOF
   %{ for i in range(var.control_plane_count) }
-  k8s-cp-${i + 1} ansible_host=${try(proxmox_virtual_environment_vm.k8s_cp[i].ipv4_addresses[0], "")} ansible_user=root
+  k8s-cp-${i + 1} ansible_host=${proxmox_virtual_environment_vm.k8s_cp[i].ipv4_addresses[1][0]} ansible_user=ansible_user
   %{ endfor }
   %{ for i in range(var.worker_count) }
-  k8s-worker-${i + 1} ansible_host=${try(proxmox_virtual_environment_vm.k8s_worker[i].ipv4_addresses[0], "")} ansible_user=root
+  k8s-worker-${i + 1} ansible_host=${proxmox_virtual_environment_vm.k8s_worker[i].ipv4_addresses[1][0]} ansible_user=ansible_user
   %{ endfor }
   [kube_control_plane]
   %{ for i in range(var.control_plane_count) }
