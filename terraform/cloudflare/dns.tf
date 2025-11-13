51eadba50b2363f731783f7cf64c7d970a1d222d
Philipp Matthias Hauptmann <haupha08@gmail.com>
2025-11-13 19:21:36 +0100

    Update.

--- terraform/cloudflare/dns.tf
+++ terraform/cloudflare/dns.tf
@@ -92,3 +92,16 @@ resource "cloudflare_dns_record" "a_records_hauptmann_dev" {
   ttl     = 1
   proxied = true
 }
+
+################################
+#  CNAME records for backend   #
+################################
+resource "cloudflare_dns_record" "cname_backend_records" {
+  for_each = var.cname_backend_records
+  zone_id = var.CLOUDFLARE_ZONE_ID_HAUPTMANN_DEV
+  name    = each.key
+  content = "${var.CLOUDFLARE_TUNNEL_STRASSGANG_ID}.cfargotunnel.com"
+  type    = "CNAME"
+  ttl     = 1
+  proxied = true
+}
